68317cfc7847a8a374062b10b2373c39383bb47d
tubanursmsk <tubanursmsk@gmail.com>
2025-06-19 10:09:28 +0300

    js

--- frontEnd/Days_6/js/site.js
+++ frontEnd/Days_6/js/site.js
@@ -43,14 +43,16 @@ function getNameVal(){
     console.log(nameVal, titleVal)
 }
 
+
 function pullList() {
     var html = ''
     var liHtml = ''
-    const citiesArr = ['Ankara', 'İstanbul', 'İzmir', 'Bursa', 'Antalya', 'Adana']
+    var selectCity = 'İstanbul'
+    const citiesArr = ['Ankara', selectCity, 'İzmir', 'Bursa', 'Antalya', 'Adana']
     for (let i = 0; i < citiesArr.length; i++) {
         const item = citiesArr[i];
-        const selected = item == 'İstanbul' ? 'selected': ''
-        const active = item == 'İstanbul' ? 'active': ''
+        const selected = item == selectCity ? 'selected': ''
+        const active = item == selectCity ? 'active': ''
         html += '<option '+selected+' value="'+i+'">'+item+'</option>'
         liHtml += '<li class="list-group-item '+active+'">'+item+'</li>'
     }
@@ -58,4 +60,64 @@ function pullList() {
     const ulCitiesObj = document.getElementById('ulCities')
     citiesObj.innerHTML = html
     ulCitiesObj.innerHTML = liHtml
+    citiesObj.style.backgroundColor = 'lightblue'
 }
+
+function listReset() {
+    const citiesObj = document.getElementById('cities');
+    const ulCitiesObj = document.getElementById('ulCities')
+    citiesObj.innerHTML = ''
+    ulCitiesObj.innerHTML = ''
+}
+
+//object
+function getTable() {
+    const user1 = {
+        id: 100,
+        name: 'Ali',
+        surname: 'Bilmem',
+        email: 'ali@mail.com',
+        status: true,
+        colors: [
+            "red", "black", "green"
+        ],
+        address: {
+            city: 'İstanbul',
+            area: 'Marmara',
+            zip: 34400
+        }
+    }
+    const arr = dataResult()
+    const newArr = arr.sort((a, b) => a.id - b.id)
+    var html = ''
+    for (let i = 0; i < newArr.length; i++) {
+        const item = newArr[i];
+        html += `
+        <tr>
+            <th scope="row">`+item.id+`</th>
+            <td>`+item.name+`</td>
+            <td>`+item.surname+`</td>
+            <td>`+item.status+`</td>
+            <td>`+item.age+`</td>
+        </tr>`
+    }
+    const tableObj = document.getElementById('tableData')
+    tableObj.innerHTML = html    
+}
+
+function dataResult() {
+    var arr = []
+    for (let i = 0; i < 10; i++) {
+        const obj = {
+            id: Math.round(Math.random() * 100),
+            name: 'Name-'+i,
+            surname: 'Surname-'+i,
+            status: i %2 === 0 ? true : false,
+            age: i
+        }
+        arr.push(obj) // dizi içine veri ekle
+    }
+    return arr
+}
+getTable()
+
