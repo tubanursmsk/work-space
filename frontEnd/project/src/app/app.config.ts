de56aa01551ffe30e5f5c4838ef52a827d1d1308
tubanursmsk <tubanursmsk@gmail.com>
2025-07-25 14:24:14 +0300

    ..

--- frontEnd/project/src/app/app.config.ts
+++ frontEnd/project/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
@@ -7,6 +8,13 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+     provideHttpClient()
   ]
 };
+/*🧠 Neden Oluyor? 
+import { provideHttpClient } from '@angular/common/http'; bu kodu neden ekledik?
+
+Angular 15+ ile gelen standalone mimaride artık NgModules yerine provideXXX sistemleri kullanılıyor.
+ Yani HttpClientModule yerine provideHttpClient() kullanılmalı. Klasik Angular projesinde AppModule 
+ içine HttpClientModule eklersin, ama burada app.config.ts içinde provideHttpClient() yapmalısın.*/
