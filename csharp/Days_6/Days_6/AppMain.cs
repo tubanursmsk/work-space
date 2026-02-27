2246250f48624cb2e1ebf6fd84fca13bcfa37ecf
tubanursmsk <tubanursmsk@gmail.com>
2026-02-28 00:46:08 +0300

    notlar

--- csharp/Days_6/Days_6/AppMain.cs
+++ csharp/Days_6/Days_6/AppMain.cs
@@ -7,10 +7,18 @@ namespace Days_6
         {
             // Action -> Sınıf
             // action -> nesne
-            // new -> aynısının kopyası - bellek
+            // new -> aynısının kopyası - yani (ramde) bellek seviyesinde. new anahtar keimesi
+                                        /* olmadan sınıfın nesnesi oluşturulamaz. nesne sayesinde sınıfın içindeki
+                                         özelliklere ve methodlara erişebiliriz. mesela action.StringValid
+                                         gibi bu bellek kullanımında tasarruf sağlar*/
             // Action(); -> kurucu method
             Action action = new Action();
-            object action1 = new Action();
+            object action1 = new Action(); /* temelde her sınıf obje sınıfından türetildiği için
+                                            böyle de tanımlanabilir ama bu şekilde tanımlamak
+                                            pek tercih edilmez çünkü action1 üzerinden Action
+                                            sınıfının özelliklerine ve methodlarına erişemeyiz
+                                            yani action1. yazdığımızda sadece defaultta atanan
+                                            obje değereleri gelir.*/
             string name = "34564564f45645";
             object surname = "Bilmem";
             action.Fibonacci(10);
@@ -21,7 +29,7 @@ namespace Days_6
             {
                 object item = arr[i];
                 Console.WriteLine(item);
-                if (item is Action)
+                if (item is Action) // item için tür Action sınıfından ise... koşulunu sağla
                 {
                     Action itemAction = (Action)item;
                 }
